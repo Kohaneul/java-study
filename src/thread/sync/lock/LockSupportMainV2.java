e750269a6ecdc6ee85cd1d22282b9839ba4fd331
hanul1106 <151487414+hanul1106@users.noreply.github.com>
2025-05-11 21:42:19 +0900

    0511 commit

--- src/thread/sync/lock/LockSupportMainV2.java
+++ src/thread/sync/lock/LockSupportMainV2.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.LockSupport;
 import static thread.util.MyLogger.log;
 import static thread.util.ThreadUtils.sleep;
 
-public class LockSupportMainV1 {
+public class LockSupportMainV2 {
     public static void main(String[] args) {
         Thread thread1 = new Thread(new ParkTest(),"Thread-1");
         thread1.start();
@@ -13,8 +13,6 @@ public class LockSupportMainV1 {
         sleep(1000);
         log("Thread-1 state : "+thread1.getState());
         log("main->unpark(Thread-1)");
-//        LockSupport.unpark(thread1);    //1. unpark 사용
-        thread1.interrupt();//2. interrupt() 사용
 
     }
     static class ParkTest implements Runnable{
@@ -22,7 +20,7 @@ public class LockSupportMainV1 {
         @Override
         public void run() {
             log("park 시작");
-            LockSupport.park();//runnable->waiting
+            LockSupport.parkNanos(2000_000000);//parkNanaos 사용 -> 2초뒤 깨어남
             log("park 종료. state : "+Thread.currentThread().getState());
             log("인터럽트 상태 : "+Thread.currentThread().isInterrupted());
 
